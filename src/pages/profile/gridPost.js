fcc9f47a331e5f3895f9f06c0289d9925fe21384
nguyenvanhieu <hieunvph15760@fpt.edu.vn>
2022-10-29 13:04:55 +0700

    FBV-65 Fix profile

--- src/pages/profile/gridPost.js
+++ src/pages/profile/gridPost.js
@@ -1,33 +1,32 @@
 export default function GridPosts() {
-    return (
-      <div className="createPost">
-        <div
-          className="createPost_header"
-          style={{ justifyContent: "space-between" }}
-        >
-          <div className="left_header_grid">Posts</div>
-          <div className="flex">
-            <div className="gray_btn">
-              <i className="equalize_icon"></i>
-            </div>
-            <div className="gray_btn">
-              <i className="manage_icon"></i>
-              Manage Posts
-            </div>
+  return (
+    <div className="createPost">
+      <div
+        className="createPost_header"
+        style={{ justifyContent: "space-between" }}
+      >
+        <div className="left_header_grid">Bài viết</div>
+        <div className="flex">
+          <div className="gray_btn">
+            <i className="equalize_icon"></i>
           </div>
-        </div>
-        <div className="create_splitter"></div>
-        <div className="createPost_body grid2">
-          <div className="view_type active">
-            <i className="list_icon filter_blue"></i>
-            List view
-          </div>
-          <div className="view_type">
-            <i className="grid_icon"></i>
-            Grid view
+          <div className="gray_btn">
+            <i className="manage_icon"></i>
+            Quản lý bài viết
           </div>
         </div>
       </div>
-    );
-  }
-  
+      <div className="create_splitter"></div>
+      <div className="createPost_body grid2">
+        <div className="view_type active">
+          <i className="list_icon filter_blue"></i>
+          Danh sách lượt xem
+        </div>
+        <div className="view_type">
+          <i className="grid_icon"></i>
+          Chế độ hiển thị
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/profile/youMayknow.js
+++ src/pages/profile/youMayknow.js
@@ -6,14 +6,14 @@ export default function PplYouMayKnow() {
   return (
     <div className="pplumayknow">
       <div className="pplumayknow_header">
-        People You May Know
+        Người bạn có thể biết
         <div className="post_header_right ppl_circle hover1">
           <Dots />
         </div>
       </div>
       <div className="pplumayknow_list">
         {stories.map((item, i) => (
-          <AddFriendSmallCard item={item} />
+          <AddFriendSmallCard item={item} key={i} />
         ))}
       </div>
     </div>
